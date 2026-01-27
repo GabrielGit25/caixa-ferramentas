bb15b0674f10e9e14426cdecd3ab4d25d0ce29fb
GabrielGit25 <delimacruzgabriel@gmail.com>
2026-01-27 11:20:38 -0300

    Update caixa.ps1

--- caixa.ps1
+++ caixa.ps1
@@ -1,5 +1,2 @@
-# caixa.ps1 v2.9 - SHazam 🔥 SEM JSON, SEM ERROS, PS5.1
-$RepoUrl = "https://raw.githubusercontent.com/GabrielGit25/caixa-ferramentas/main"
-
-# AUTO-INSTALAÇÃO
-if (!(Test-Path $PROFILE)) { New-Item -Path $PROFILE
+# caixa.ps1 v2.9 - SHazam 🔥 ZERO ERROS PS5.1
+$RepoUrl
